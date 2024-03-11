b81a022473b8571c15e037733748f272d98e7170
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-03-11 14:13:58 -0300

    Terminando de organizar o código

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const imagemin = require('gulp-imagemin')
 const uglify = require('gulp-uglify')
 
 function Sass() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./build/css'))
+}
+
+function images(){
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'))
 }
 
 function scripts(){
@@ -18,4 +25,4 @@ function Watch() {
     gulp.watch('./src/styles/**/*.scss', Sass)
 }
 
-exports.default = gulp.parallel(Sass, Watch, scripts)
+exports.default = gulp.parallel(Sass, Watch, scripts, images)
